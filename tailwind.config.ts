bfc32c96fcf82dda9c5cd18770925317cc7bdcaa
MahnoorFatimaArshad <158273911+mahnoorfatimaahmad@users.noreply.github.com>
2024-05-11 18:32:20 -0700

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customColor: {
+          primary: "#24f8c7", // Define your custom color here
+          // Add more custom color variations as needed
+        },
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
